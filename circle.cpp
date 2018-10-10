0cebfc547f71665f92f1d031414426f0b3f7ec6f
Isaac <saacman27@gmail.com>
2018-10-10 16:57:30 -0500

    Clase Circle sin modificaciones

--- circle.cpp
+++ circle.cpp
@@ -7,8 +7,8 @@ Circle::Circle( Point center, double radius, Color fill ) {
 
    m_center = center;
    m_radius = radius;
-   //m_fill = fill;
-   set_fill(fill);
+   m_fill = fill;
+   
 }
 
 // accessors
@@ -20,22 +20,22 @@ double Circle::radius() const {
    return m_radius;
 }
 
-// Color Circle::fill() const {
-//    return m_fill;
-// }
+Color Circle::fill() const {
+   return m_fill;
+}
 
-// OVERRIDE
+
 // interfaz común con otras figuras
-// bool Circle::test( Point pt ) {
-//    double d2;
-//    // distancia = (x - x_c)^2 + (y - y_c)^2
-//    d2 =  (m_center.x - pt.x)*(m_center.x - pt.x);
-//    d2 += (m_center.y - pt.y)*(m_center.y - pt.y);
-//    if ( d2 <= m_radius*m_radius ) return true;
-//    return false;
-// }
-//
-// Color Circle::ptcolor( Point pt ) {
-//    if ( test(pt) ) return m_fill;
-//    return Color(CLEAR);
-// }
+bool Circle::test( Point pt ) {
+   double d2;
+   // distancia = (x - x_c)^2 + (y - y_c)^2
+   d2 =  (m_center.x - pt.x)*(m_center.x - pt.x);
+   d2 += (m_center.y - pt.y)*(m_center.y - pt.y);
+   if ( d2 <= m_radius*m_radius ) return true;
+   return false;
+}
+
+Color Circle::ptcolor( Point pt ) {
+   if ( test(pt) ) return m_fill;
+   return Color(CLEAR);
+}
